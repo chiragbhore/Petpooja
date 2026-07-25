a52a95a42d03dc278d03e917be1835612b778c17
chiragbhore <chirag.bhore@petpooja.com>
2026-07-25 10:44:30 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,9 @@ export default function App({ Component, pageProps }) {
         <title>PitchLab — Petpooja Sales Training</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script dangerouslySetInnerHTML={{ __html: setThemeScript }} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
